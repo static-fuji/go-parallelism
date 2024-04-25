7d8ec8b6d398f9e77e3c677f4e93553dd3cac5b3
static-fuji <jyun1228haru@icloud.com>
2024-04-25 11:57:37 +0900

    [update] mapをチャネルに送信

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"bufio"
-	"crypto/sha256"
 	"fmt"
 	"os"
-	"sync"
 )
 
 func main() {
 	// ファイル名を指定
-	filename := "example.txt"
+	filename := "test.txt"
 
 	// ファイルをオープン
 	file, err := os.Open(filename)
@@ -23,39 +21,30 @@ func main() {
 	// ファイルからScannerを作成
 	scanner := bufio.NewScanner(file)
 
-	// ワーカーゴルーチンの数を定義
-	numWorkers := 5
+	// マップを作成して行を格納
+	ch := make(chan map[int]string)
+	lineMap := make(map[int]string)
+	lineNumber := 1
 
-	// チャネルを作成してワーカーゴルーチンとメインゴルーチンが情報を共有する
-	lines := make(chan string)
-	var wg sync.WaitGroup
-
-	// ワーカーゴルーチンを起動
-	for i := 0; i < numWorkers; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for line := range lines {
-				hash := sha256.Sum256([]byte(line))
-				fmt.Printf("'%s' のSHA256ハッシュ: %x\n", line, hash)
-			}
-		}()
-	}
-
-	// ファイルから一行ずつ読み込んでチャネルに送信
 	for scanner.Scan() {
 		line := scanner.Text()
-		lines <- line
+		lineMap[lineNumber] = line
+		lineNumber++
 	}
-
-	// チャネルを閉じてゴルーチンが終了するようにする
-	close(lines)
-
-	// 全てのワーカーゴルーチンが終了するのを待つ
-	wg.Wait()
 
 	// エラーのチェック
 	if err := scanner.Err(); err != nil {
 		fmt.Println("ファイルの読み込み中にエラーが発生しました:", err)
+		return
 	}
+
+	// マップの内容を出力
+	for lineNumber, line := range lineMap {
+		go func() {
+			fmt.Println(lineNumber, line, lineMap)
+			ch <- lineMap
+		}()
+		<-ch
+	}
+	close(ch)
 }
